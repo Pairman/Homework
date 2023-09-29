812c77c1f9919d30cb26d5a17dc07aca2c28c21a
Null <pairmanxlr@gmail.com>
2023-09-29 23:56:52 +0800

    Add files via upload

--- Data-Visualization/2/csv_json_converter.py
+++ Data-Visualization/2/csv_json_converter.py
@@ -1,12 +1,15 @@
 import csv
 import json
 
-data = [["Year", "Month and Day", "Temperature"]]
-
+data = []
 with open("daily-minimum-temperatures-in-me.csv", "r") as csv_input:
 	reader = csv.reader(csv_input)
 	for row in reader:
 		data.append([row[0][0:4], row[0][5:10], row[1]])
 
 with open("daily-minimum-temperatures-in-me.js", "w") as json_output:
+        json_output.write("var data = ")
+
+data[0] = ["Year", "Month and Day", "Temperature"]
+with open("daily-minimum-temperatures-in-me.js", "a") as json_output:
 	json.dump(data, json_output, indent = "  ")
